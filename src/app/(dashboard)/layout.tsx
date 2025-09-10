b4b30eb0f7dd6cf19be7e53db5fb9b0d249ebc07
Alisha Fatima <alishafatima6768@gmail.com>
2025-09-10 13:29:08 +0500

    nextjs responsive navbar design

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,27 +1,31 @@
 import Link from "next/link";
 import Image from "next/image";
 import Menu from "@/components/Menu";
-
+import Navbar from "@/components/Navbar";
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-      <div className="h-screen flex">
-        {/* {left} */}
-        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4">
-          <Link href="/"
-         className="flex item-center justify-center lg:justify-start gap-2">
+    <div className="h-screen flex">
+      {/* {left} */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]  p-4">
+        <Link
+          href="/"
+          className="flex item-center justify-center lg:justify-start gap-2"
+        >
           <Image src="/logo.png" alt="Logo" width={32} height={32} />
           <span className="hidden lg:block">SchoolLama</span>
         </Link>
-        <Menu/>
-        </div>
-        
-        {/* {right} */}
-        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">r</div>
-
+        <Menu />
       </div>
+
+      {/* {right} */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F78FA] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
+    </div>
   );
 }

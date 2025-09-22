21b2e32e6bec675f6c3d2561983948c1a45e6fee
Alisha Fatima <alishafatima6768@gmail.com>
2025-09-22 23:41:51 +0500

    rendering forom conditionaliy in react

--- src/components/forms/TeacherForm.tsx
+++ src/components/forms/TeacherForm.tsx
@@ -1,59 +1,143 @@
-"use client"
+"use client";
 
-import { zodResolver } from '@hookform/resolvers/zod';
-import React from 'react'
-import { useForm } from 'react-hook-form';
-import { z } from 'zod'; // or 'zod/v4'
-
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod"; // or 'zod/v4'
+import InputField from "../InputField";
 const schema = z.object({
-username: z
-.string()
-.min(3, { message: 'username must be atleast three character long!' })
-.max(20, { message: 'username must be atleast three character long!' }),
+  username: z
+    .string()
+    .min(3, { message: "username must be atleast three character long!" })
+    .max(20, { message: "username must be atleast three character long!" }),
 
-    email: z.string().email({ message: "Invalid email address"}),
-    password:z.string().min(8 ,{message:"password must be atleast 8 character long"}),
-    firstName:z.string().min(1,{message:"First Name is required"}),
-    lastName:z.string().min(1,{message:"Last name is required"}),
-    phone:z.string().min(1 ,{message:"Phone is required"}),
-    address:z.string().min(1 ,{message:"Adress is required"}),
-    birthday:z.date({message:"Birthday is required"}),
-    sex:z.enum(["male" , "female"],{message:"Sex is required"}),
-    img:z.instanceof(File ,{message:"Image is required"}),
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(8, { message: "password must be atleast 8 character long" }),
+  firstName: z.string().min(1, { message: "First Name is required" }),
+  lastName: z.string().min(1, { message: "Last name is required" }),
+  phone: z.string().min(1, { message: "Phone is required" }),
+  address: z.string().min(1, { message: "Adress is required" }),
+bloodType: z.string().min(1, { message: "Blood Type is required" }),
 
-
+  birthday: z.date({ message: "Birthday is required" }),
+  sex: z.enum(["male", "female"], { message: "Sex is required" }),
+  img: z.instanceof(File, { message: "Image is required" }),
 });
-
-
-const TeacherForm = ({ type, data }: {
-    type: "create" | "update";
-    data?: any;
+type Inputs = z.infer<typeof schema>;
+const TeacherForm = ({
+  type,
+  data,
+}: {
+  type: "create" | "update";
+  data?: any;
 }) => {
-
-    const {
+  const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({
+  } = useForm<Inputs>({
     resolver: zodResolver(schema),
   });
 
-  const onSubmit = handleSubmit((data)=>{
-      console.log(data)
-  })
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
 
-  
-    return (
-        <form className='flex flex-col gap-8' onSubmit={onSubmit}>
-            <h1 className='text-xl font-semibold'>Create a new teacher</h1>
-            <span className='text-xs text-gray-400 font-medium'>Authentication Infomration</span>
-            <input type='text' {...register("username")} className='ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm'/>
-            {errors.username?.message && <p className='text-red-400 text-xs'>{errors.username?.message.toString()}</p>}
-            <span className='text-xs text-gray-400 font-medium'>Personal Infomration</span>
-            <button className='bg-blue-400 text-white p-2 rounded-md'>{type === 'create' ? "Create" : "Update"}</button>
+  return (
+    <form className="flex flex-col gap-8" onSubmit={onSubmit}>
+      <h1 className="text-xl font-semibold">Create a new teacher</h1>
+      <span className="text-xs text-gray-400 font-medium">
+        Authentication Infomration
+      </span>
+      <InputField
+        label="Username"
+        name="username"
+        defaultValue={data?.username}
+        register={register}
+        error={errors?.username}
+        inputProps={{}}
+      />
 
-        </form>
-    )
-}
+        <InputField
+        label="Email"
+        name="email"
+        type="email"
+        defaultValue={data?.email}
+        register={register}
+        error={errors?.email}
+        inputProps={{}}
+      />  
+      
+      <InputField
+        label="Password"
+        name="password"
+        type="password"
+        defaultValue={data?.password}
+        register={register}
+        error={errors?.password}
+        inputProps={{}}
+      />
+  <InputField
+        label="First Name"
+        name="firstName"
+        defaultValue={data?.firstName}
+        register={register}
+        error={errors?.firstName}
+        inputProps={{}}
+      />
+        <InputField
+        label="Last Name"
+        name="lastName"
+        defaultValue={data?.lastName}
+        register={register}
+        error={errors?.lastName}
+        inputProps={{}}
+      />
+        <InputField
+        label="Phone"
+        name="phone"
+        defaultValue={data?.phone}
+        register={register}
+        error={errors?.phone}
+        inputProps={{}}
+      />
+        <InputField
+        label="Address"
+        name="address"
+        defaultValue={data?.address}
+        register={register}
+        error={errors?.address}
+        inputProps={{}}
+      />  
+      
+      <InputField
+        label="Blood Type"
+        name="bloodType"
+        defaultValue={data?.bloodType}
+        register={register}
+        error={errors?.bloodType}
+        inputProps={{}}
+      />
 
-export default TeacherForm
+        <InputField
+        label="Birthday"
+        name="birthday"
+        defaultValue={data?.birthday}
+        register={register}
+        error={errors?.birthday}
+        type="date"
+        inputProps={{}}
+      />
+      <span className="text-xs text-gray-400 font-medium">
+        Personal Infomration
+      </span>
+      <button className="bg-blue-400 text-white p-2 rounded-md">
+        {type === "create" ? "Create" : "Update"}
+      </button>
+    </form>
+  );
+};
+
+export default TeacherForm;
